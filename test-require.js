f5d88b0b928523a69d79fa60c4c79c974b13f2de
Professor <professor@exemplo.com>
2026-06-16 19:31:34 -0300

    chore: remove temp scripts and json files from git

--- test-require.js
+++
@@ -1,6 +0,0 @@
-try {
-    const admin = require("firebase-admin");
-    console.log("firebase-admin version:", admin.SDK_VERSION || "loaded");
-} catch (e) {
-    console.error("Require failed:", e.message);
-}
